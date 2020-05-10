b371ccbbb1500cc8b76ee80d7e607572c8dad2d5
Carlos Muñoz <carloxmunozr.12@gmail.com>
2020-05-10 00:15:29 -0500

    test deploy

---
+++ precache-manifest.9d54f03d0a3d8a84d42fa7f47921e741.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "694a0d03e4e8bcb8adc1f66dbfed6adf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33377fbaf1a80c492ca4",
+    "url": "/static/css/main.bfa2d19f.chunk.css"
+  },
+  {
+    "revision": "bc55bc8569512b7a26a4",
+    "url": "/static/js/2.e9112bfc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e9112bfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33377fbaf1a80c492ca4",
+    "url": "/static/js/main.2191d6d4.chunk.js"
+  },
+  {
+    "revision": "432fb486ba942574f614",
+    "url": "/static/js/runtime-main.0d9708cb.js"
+  },
+  {
+    "revision": "8b4d6e74cffa8b09d37d84376cd22e12",
+    "url": "/static/media/check.8b4d6e74.svg"
+  },
+  {
+    "revision": "443014aa774223219515b33d7ac5f396",
+    "url": "/static/media/correct.443014aa.svg"
+  },
+  {
+    "revision": "ad7df54991cf24e0d3f10458e209eb79",
+    "url": "/static/media/domain.ad7df549.svg"
+  },
+  {
+    "revision": "27d7e7bcbf7e7a7b896305e22b022e8e",
+    "url": "/static/media/electronics.27d7e7bc.svg"
+  },
+  {
+    "revision": "ca41c76d8b2cd1392da8e0a0692db7ad",
+    "url": "/static/media/no-check.ca41c76d.svg"
+  },
+  {
+    "revision": "599fcb61df7ecae946687812b0bf6c84",
+    "url": "/static/media/wrong.599fcb61.svg"
+  }
+]);
